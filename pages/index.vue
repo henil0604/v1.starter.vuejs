1a3dc849437c6538c19a64f8a07f8dae2de6ccc6
henil0604 <henilmalaviya06@gmail.com>
2022-02-06 15:06:38 +0530

    Init

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-result
+    status="success"
+    title="Successfully Cloned Starter Kit"
+    sub-title="This Vue Starter kit is made for personal use, so if you want to use it, use it at your own risk. Edit the File to view the changes."
+  >
+  </a-result>
+</template>
+
+<script>
+  import Network from '@/modules/Network';
+
+  export default {
+    name: 'IndexPage',
+  }
+
+</script>
